b24750ca869c48ad6c617068342e823d84e352bb
Yuen Shing <y.shing@me.com>
2015-08-08 12:08:34 +0800

    Upgrade AdminLTE module Upgrade Bootstrap to 3.3.4

--- client/app/modules/core/directives/navbar.js
+++ client/app/modules/core/directives/navbar.js
@@ -10,6 +10,6 @@ angular.module('com.module.core')
   .directive('navbar', function() {
     return {
       templateUrl: 'modules/core/views/elements/navbar.html',
-      restrict: 'E'
+      restrict: 'A'
     };
   });
